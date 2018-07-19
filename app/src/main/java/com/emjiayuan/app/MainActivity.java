f35afef2562cc35dff0c6e4c938f05b21c4ee95b
 youlai <761364115@qq.com>
2018-07-19 14:37:53 +0800

    首次
    Signed-off-by:  youlai < 761364115@qq.com>

--- app/src/main/java/com/emjiayuan/app/MainActivity.java
+++ app/src/main/java/com/emjiayuan/app/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
     private CommunityFragment2 communityFragment;
     public static ShoppingCarFragment shoppingCarFragment;
     private PersonalFragment personalFragment;
+//    private PersonalFragment personalFragment;
 
 
 
